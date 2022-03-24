bfe50fde535f8bcf6fe34ab4c79ee4552f8c283f
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-03-24 22:02:42 +0530

    Added::Program to print the dublicate elements of an array.

---
+++ Dublicate_Array.java
@@ -0,0 +1,25 @@
+/*@Purpose::Java Program to print the duplicate elements of an array.
+ *@File::Java Assignment
+ *@Author::Kishlay Kishan
+ */
+
+package Java_Assignment_Day3;
+
+public class Dublicate_Array {
+
+	public static void main(String[] args) {
+		// Initialize array
+		int[] arr = new int[] { 1, 2, 3, 4, 2, 7, 8, 8, 3 };
+
+		System.out.println("Duplicate elements in given array: ");
+		// Searches for duplicate element
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j])
+					System.out.println(arr[j]);
+			}
+		}
+
+	}
+
+}
